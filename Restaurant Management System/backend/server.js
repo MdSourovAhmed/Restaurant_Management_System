6aae8bd9a3c0e11f7c774b15295c3e8ea3661c34
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-10-04 23:36:02 +0600

    Update server.js

--- Restaurant Management System/backend/server.js
+++ Restaurant Management System/backend/server.js
@@ -11,23 +11,9 @@ dotenv.config();
 const server = express();
 connectDB();
 
-
-
-
-
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-
-
 server.use(express.json());
 server.use(cors())
 
-// // MongoDB Connection
-// mongoose.connect('mongodb://localhost/restaurant', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => console.log('MongoDB connected')).catch(err => console.error(err));
 
 // Routes
 server.use('/api/admin/auth',require('./src/routes/AdminRoutes/adminSetup'))
@@ -44,42 +30,6 @@ server.use('/api/menu', require('./src/routes/AdminRoutes/menuRoutes'));
 server.use('/api/orders', require('./src/routes/AdminRoutes/orderRoutes'));
 server.use('/api/tables', require('./src/routes/AdminRoutes/tableRoutes'));
 server.use('/api/reservations', require('./src/routes/AdminRoutes/reservationRoutes'));
-// const PORT = process.env.PORT || 5000;
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-
-
-// // Security Middlewares
-// server.use(helmet());
-// server.use(cors({
-//   origin: process.env.FRONTEND_URL || 'http://localhost:5173/',
-//   credentials: true
-// }));
-// server.use(express.json({ limit: '10kb' }));
-// server.use(express.urlencoded({ extended: true, limit: '10kb' }));
-// server.use(hpp());
-// server.use((req, res, next) => {
-//   req.body = mongoSanitize(req.body);
-//   req.query = mongoSanitize(req.query);
-//   req.params = mongoSanitize(req.params);
-//   next();
-// });
-
-// const globalLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 100,
-//   message: 'Too many requests, please try again later.'
-// });
-// server.use(globalLimiter);
-
-// // Routes
-// server.use('/api/events', require('./src/routes/eventRoutes'));
-// server.use('/api/registrations', require('./src/routes/registrationRoutes'));
-// server.use('/api/auth', require('./src/routes/userRoutes'));
-// server.use('/api/auth/admin', require('./src/routes/adminSetup')); // New admin routes
-// server.use('/api/admin', require('./src/routes/adminRoutes')); // New admin routes
 
 // Error Handling
 server.use((err, req, res, next) => {
