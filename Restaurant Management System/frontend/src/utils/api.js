7327119e2197bfe1d499c5b62386dc476f088ff1
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-10-08 23:11:19 +0600

    Update api.js

--- Restaurant Management System/frontend/src/utils/api.js
+++ Restaurant Management System/frontend/src/utils/api.js
@@ -33,7 +33,6 @@
 //     return Promise.reject(error);
 //   }
 // );
-
 // export default api;
 
 import axios from "axios";
@@ -77,24 +76,3 @@ api.interceptors.response.use(
 );
 
 export default api;
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token');
-//       window.location.href = '/login';
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
